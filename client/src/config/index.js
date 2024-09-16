b3ad693919bcc2275021b9854227d94b9e52671f
Adesh Jadhav <adejad1234@gmail.com>
2024-09-16 10:44:02 +0530

    Done with Login part

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -21,3 +21,22 @@ export const registerFormControl = [
         type: 'password'
     }
 ]
+
+
+
+export const loginFormControl = [
+    {
+        name: 'email',
+        label: 'Email',
+        placeholder: 'Enter your email',
+        componentType: 'input',
+        type: 'email'
+    },
+    {
+        name: 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        componentType: 'input',
+        type: 'password'
+    }
+]
--- client/src/pages/auth/login.jsx
+++ client/src/pages/auth/login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import CommonForm from '@/components/common/form'
+import { loginFormControl } from '@/config'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+
+const initialState = {
+  email: '',
+  password: ''
+}
 
 const AuthLogin = () => {
+
+  const [formData, setFormData] = useState(initialState);
+
+  function onSubmit() {
+    console.log('OnSubmit click');
+  }
+
+
   return (
-    <div>AuthLogin</div>
-  )
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign In
+        </h1>
+        <p className="mt-2">
+          Don't have an account
+          <Link
+            className="font-medium ml-2 text-primary hover:underline"
+            to="/auth/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+
+      <CommonForm
+        formControls={loginFormControl}
+        buttonText="Sign In"
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 }
 
 export default AuthLogin
