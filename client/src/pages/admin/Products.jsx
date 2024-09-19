190c10d4de523bb97f251ba01466bb44238128e3
Adesh Jadhav <adejad1234@gmail.com>
2024-09-19 14:55:23 +0530

    WIP: cloudnary for uploading images

--- client/src/pages/admin/Products.jsx
+++ client/src/pages/admin/Products.jsx
@@ -21,31 +21,49 @@ const initialFormData = {
 const AdminProducts = () => {
   const [openCreateProductsDialog, setOpenCreateProductsDialog] = useState(false);
   const [formData, setFormData] = useState(initialFormData);
+  const [imageFile, setImageFile] = useState(null);
+  const [lastUploadedImageURL, setLastUploadedImageURL] = useState('');
 
-  const onSubmit = () => {
+  function  onSubmit() {
     console.log('Product Added ');
   }
 
   return (
     <>
-      <div className='w-full mb-3 flex justify-end'>
-        <Button onClick={()=> setOpenCreateProductsDialog(true)}>Add New Product</Button>
+      <div className="w-full mb-3 flex justify-end">
+        <Button onClick={() => setOpenCreateProductsDialog(true)}>
+          Add New Product
+        </Button>
       </div>
 
-      <div className='grid gap-4 md:grid-cols-3 lg: grid-cols-4'></div>
-      <Sheet open={openCreateProductsDialog} onOpenChange={()=> setOpenCreateProductsDialog(false)}>
-        <SheetContent side='right' className='overflow-auto'>
+      <div className="grid gap-4 md:grid-cols-3 lg: grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductsDialog}
+        onOpenChange={() => setOpenCreateProductsDialog(false)}
+      >
+        <SheetContent side="right" className="overflow-auto">
           <SheetHeader>
             <SheetTitle>Add New Product</SheetTitle>
           </SheetHeader>
-          <ProductImageUpload />
-          <div className='py-6'>
-            <CommonForm formControls={addProductsFormElements} formData={formData} setFormData={setFormData} buttonText='Add' onSubmit={onsubmit} />
+          <ProductImageUpload
+            imageFile={imageFile}
+            setImageFile={setImageFile}
+            lastUploadedImageURL={lastUploadedImageURL}
+            setLastUploadedImageURL={setLastUploadedImageURL}
+          />
+          <div className="py-6">
+            <CommonForm
+              formControls={addProductsFormElements}
+              formData={formData}
+              setFormData={setFormData}
+              buttonText="Add"
+              onSubmit={onsubmit}
+            />
           </div>
         </SheetContent>
       </Sheet>
     </>
-  )
+  );
 }
 
 export default AdminProducts
