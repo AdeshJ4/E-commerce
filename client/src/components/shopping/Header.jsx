90300887ce5c0e5e65961a35d723e502b989f1d8
Adesh Jadhav <adejad1234@gmail.com>
2024-09-24 15:10:01 +0530

    WIP: Header

--- client/src/components/shopping/Header.jsx
+++ client/src/components/shopping/Header.jsx
@@ -1,9 +1,18 @@
+import { House } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ShoppingHeader = () => {
   return (
-    <div>ShoppingHeader</div>
-  )
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to='/shop/home' className='flex items-center gap-2'>
+          <House className="h-6 w-6 " />
+          <span className='font-bold'>E-Commerce</span>
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default ShoppingHeader
